0683492a835594baa98128f4c6fe2039168ae04e
sophia chioma <sophiachioma@gmail.com>
2022-06-26 12:47:08 +0100

    changes made in setting.py

--- I4G003669XSX/blog/views.py
+++ I4G003669XSX/blog/views.py
@@ -1,3 +1,39 @@
+from re import template
 from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    CreateView,
+    DetailView,
+    UpdateView,
+    DeleteView
+)
+from django.urls import reverse_lazy
+from .models import Post
+
 
 # Create your views here.
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+    template_name = 'post_form.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+    template_name = 'base.html'
+
+class PostDeletView(DeleteView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+    template_name = 'post_confirm_delete.html'
